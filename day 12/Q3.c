5f54365afd4d4672b224bf7c56a8218e8987e6d4
amankumar2522 <amank111572@gmail.com>
2026-07-08 13:27:49 +0530

    Uploaded files

---
+++ day 12/Q3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to print Fibonacci series
+void fibonacci(int n)
+{
+    int a = 0, b = 1, c, i;
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
+
+int main()
+{
+    int terms;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &terms);
+
+    printf("Fibonacci series: ");
+    fibonacci(terms);
+
+    return 0;
+}
